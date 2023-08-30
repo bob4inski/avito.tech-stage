ddbec030173b509b3009a6d5259eee27205800f3
bob4inski <robertstryzhak@yandex.ru>
2023-08-30 22:24:23 +0300

    Add server.SetHandler

--- app/code/go.mod
+++ app/code/go.mod
@@ -1,6 +1,6 @@
 module main
 
-go 1.21.0
+go 1.21
 
 require github.com/go-redis/redis v6.15.9+incompatible
 
--- app/code/main.go
+++ app/code/main.go
@@ -1,18 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
-	"encoding/json"
-	"github.com/go-redis/redis"
+
 	"github.com/go-chi/chi"
+	"github.com/go-redis/redis"
 )
 
 // Service is an interface that defines the required functions for handlers.
 type Service interface {
 	Get(key string) (string, error)
-	Del(key string)  (string, error)
+	Del(key string) (string, error)
+	Set(key string, value string) (string, error)
 }
 
 // ServiceImpl is a struct that implements the Service interface.
@@ -31,17 +33,26 @@ func (s *ServiceImpl) Get(key string) (string, error) {
 	return value, nil
 }
 
-
 func (s *ServiceImpl) Del(key string) (string, error) {
 
-	_ , err := s.redisClient.Del(key).Result()
+	_, err := s.redisClient.Del(key).Result()
 	if err == redis.Nil {
 		return "", fmt.Errorf("Key not found")
 	} else if err != nil {
 		return "", err
-	} 
+	}
 
 	return "ok", nil
+}
+
+func (s *ServiceImpl) Set(key string, value string) (string, error) {
+
+	set_result, err := s.redisClient.Set(key, value, 0).Result()
+
+	if err != nil {
+		return "", err
+	}
+	return set_result, nil
 }
 
 // Server is a struct that represents the HTTP server.
@@ -82,19 +93,17 @@ func (s *Server) GetHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func (s *Server) DelHandler(w http.ResponseWriter, r *http.Request) {
-	var payload map[string]interface{}
+	var del_payload map[string]string
 
-	err := json.NewDecoder(r.Body).Decode(&payload)
+	err := json.NewDecoder(r.Body).Decode(&del_payload)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
-		return 
+		return
 	}
 
-
-	if len(payload) == 1 {
-		for key, _ := range payload {
-			_ , err := s.service.Del(key)
-
+	if len(del_payload) == 1 {
+		for key, _ := range del_payload {
+			_, err := s.service.Del(key)
 			if err == redis.Nil {
 				w.WriteHeader(http.StatusNotFound)
 				fmt.Fprint(w, "key does not exist")
@@ -104,7 +113,7 @@ func (s *Server) DelHandler(w http.ResponseWriter, r *http.Request) {
 				http.Error(w, "Internal Server Error", http.StatusInternalServerError)
 			}
 		}
-	
+
 	} else {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprint(w, "Wrong request")
@@ -116,6 +125,36 @@ func (s *Server) DelHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func (s *Server) SetHandler(w http.ResponseWriter, r *http.Request) {
+
+	var payload map[string]string
+
+	err := json.NewDecoder(r.Body).Decode(&payload)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	if len(payload) == 1 {
+		for key, value := range payload {
+
+			set_result, err := s.service.Set(key, value)
+
+			if err != nil {
+				log.Println(err)
+				http.Error(w, "Internal Server Error while set", http.StatusInternalServerError)
+				return
+			} else {
+				w.WriteHeader(http.StatusOK)
+				fmt.Fprintf(w, "%s : %s", "Запись в базу данных прошла успешно", set_result)
+			}
+		}
+
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Wrong request")
+	}
+}
 
 func main() {
 	rdb := redis.NewClient(&redis.Options{
@@ -133,6 +172,7 @@ func main() {
 	r := chi.NewRouter()
 	r.Get("/get", server.GetHandler)
 	r.Delete("/del", server.DelHandler)
+	r.Post("/set", server.SetHandler)
 
-	log.Fatal(http.ListenAndServe(":18080", r))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
